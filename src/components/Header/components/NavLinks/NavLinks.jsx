2b13894a66477dd2092d133d67fc7f649a9b9fa8
Stas Proydakov <stasprojdakov@icloud.com>
2023-04-30 20:06:01 +0300

    fix: убрал ссылку на главную страницу на десктопе

--- src/components/Header/components/NavLinks/NavLinks.jsx
+++ src/components/Header/components/NavLinks/NavLinks.jsx
@@ -4,7 +4,7 @@ import './NavLinks.scss';
 export default function NavLinks () {
   return (
     <div className='nav-links'>
-      <NavLink to={'/'} className='link header__link'>Главная</NavLink>
+      <NavLink to={'/'} className='link header__link hidden'>Главная</NavLink>
       <NavLink to={'/movies'} className='link header__link'>Фильмы</NavLink>
       <NavLink to={'/saved-movies'} className='link header__link'>Сохраненные фильмы</NavLink>
     </div>
