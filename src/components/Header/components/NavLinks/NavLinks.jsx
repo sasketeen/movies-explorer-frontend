d1a9cb5baedadb0d62715cc663a3b6d64bf50e72
Stas Proydakov <stasprojdakov@icloud.com>
2023-04-30 23:16:11 +0300

    feat: сверстал секцию intro

--- src/components/Header/components/NavLinks/NavLinks.jsx
+++ src/components/Header/components/NavLinks/NavLinks.jsx
@@ -4,9 +4,9 @@ import './NavLinks.scss';
 export default function NavLinks () {
   return (
     <div className='nav-links'>
-      <NavLink to={'/'} className='link header__link hidden'>Главная</NavLink>
-      <NavLink to={'/movies'} className='link header__link'>Фильмы</NavLink>
-      <NavLink to={'/saved-movies'} className='link header__link'>Сохраненные фильмы</NavLink>
+      <NavLink to={'/'} className='link header__link nav-links__link nav-links__link_hidden'>Главная</NavLink>
+      <NavLink to={'/movies'} className='link header__link nav-links__link'>Фильмы</NavLink>
+      <NavLink to={'/saved-movies'} className='link header__link nav-links__link'>Сохраненные фильмы</NavLink>
     </div>
   );
 }
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,24 @@
 import MyHeader from '@c/Header/MyHeader';
 
+import './MainPage.scss';
+import mainImage from '@/assets/images/mainpage-pic.svg';
+
 export default function MainPage () {
   return (
     <>
       <MyHeader></MyHeader>
+
+      <section className='intro'>
+        <div className="container intro__container">
+          <div>
+            <h1 className='intro__title'>Учебный проект студента факультета Веб&#8209;разработки</h1>
+            <p className='intro__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя</p>
+            <a href="#about" className='intro__button-link'>Узнать больше</a>
+          </div>
+          <img src={mainImage} alt="Земной шар составленный из слов Web" className='intro__main-image'/>
+        </div>
+      </section>
+      <section id='about'></section>
     </>
   );
 }
