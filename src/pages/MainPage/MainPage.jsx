ae4c45f0967d46c36bd864ffbe644c970e881761
Stas Proydakov <stasprojdakov@icloud.com>
2023-05-01 17:27:50 +0300

    feat: сверстал секцию технологий

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -81,6 +81,23 @@ export default function MainPage () {
           </div>
         </div>
       </section>
+
+      <section className="technologies">
+        <div className="container section-container">
+          <h2 className="section-container__title">Технологии</h2>
+          <h3 className='technologies__subtitle'>7 технологий</h3>
+          <p className='technologies__paragraph'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте</p>
+          <ul className='technologies__list'>
+            <li className='technologies__list-item'>HTML</li>
+            <li className='technologies__list-item'>CSS</li>
+            <li className='technologies__list-item'>JS</li>
+            <li className='technologies__list-item'>React</li>
+            <li className='technologies__list-item'>Git</li>
+            <li className='technologies__list-item'>Express.js</li>
+            <li className='technologies__list-item'>mongoDB</li>
+          </ul>
+        </div>
+      </section>
     </>
   );
 }
