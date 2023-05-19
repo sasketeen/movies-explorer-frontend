b080c10381b11c45747d42f123af6c9a4039182a
Stas Proydakov <stasprojdakov@icloud.com>
2023-05-20 01:56:55 +0300

    fix routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -43,7 +43,7 @@ function App () {
         })
         .finally(() => setIsAppLoading(false));
     } else {
-      handleSignOut();
+      setIsUserSignIn(false);
       setIsAppLoading(false);
     }
   }, []);
